e913b612c2ed23345b63b7ff56512173ac2848ca
Abdul Haadi Bin Sakibb <abdulhaadibinsakibb1234@gmail.com>
2026-05-02 12:18:41 +0500

    Added delete and view feature for portfolio. added label that says made by rdhster and hudster to the main file

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ models.Base.metadata.create_all(bind=engine)
 @app.get("/")
 def root():
     return {"message": "Welcome to the Stock Tracker API!"}
+    return {"Made by Rdhster and Hudster"}
 
 
 
--- routes/portfolio.py
+++ routes/portfolio.py
@@ -1,5 +1,5 @@
 # route for portfolio management (add/remove stocks, view portfolio)
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import SessionLocal
 import models
@@ -14,8 +14,7 @@ def get_db():
     finally:   
         db.close()
 
-
-
+#add stock function here (takes in ticker, quantity, buy price and adds it to portfolio)
 
 @router.post("/portfolio/add")
 def add_to_portfolio(ticker:str, quantity:float, buy_price:float, db:Session=Depends(get_db)):
@@ -28,5 +27,25 @@ def add_to_portfolio(ticker:str, quantity:float, buy_price:float, db:Session=Dep
     db.commit()
     db.refresh(holding)
     return {"message": f"{quantity} shares of {ticker.upper()} have been added to your portifolio at a buy price of {buy_price}.", "id": holding.id}
+
+#delete stock function here (takes in holding id and deletes it from portfolio)
+
+
+@router.delete("/portfolio/remove/{holding_id}")
+def remove_stock(holding_id:int, db:Session=Depends(get_db)):
+    holding = db.query(models.Portfolio).filter(models.Portfolio.id == holding_id).first()
+    if holding:
+        db.delete(holding)
+        db.commit()
+        db.refresh(holding)
+        return {"Alert": f"{holding.ticker.upper()} has been removed from your portfolio."}
+    else:
+        raise HTTPException(status_code=404, detail="Holding not found")    
     
-    
+
+#view portfolio function here (returns list of all holdings in portfolio)
+
+@router.get("/portfolio")
+def get_portfolio(db: Session = Depends(get_db)):
+    holdings = db.query(models.Portfolio).all()
+    return holdings
