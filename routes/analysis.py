c3801a2c5c38c073a1183560fe3fb02431cf7bf2
Abdul Haadi Bin Sakibb <abdulhaadibinsakibb1234@gmail.com>
2026-05-04 21:59:30 +0500

    deleted unnecessary files that i made before inital commit. added sentiment scoring as well

--- routes/analysis.py
+++ routes/analysis.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from groq import Groq
 import os
 from dotenv import load_dotenv
+import requests
 
 
 load_dotenv()
@@ -14,8 +15,15 @@ client = Groq(api_key=GROQ_API_KEY)
 
 @router.get("/analysis/{ticker}")
 def analyze_stock(ticker:str):
-    '''Analyzes the specified stock ticker using the Groq API, providing insights based on historical data and predictive modeling. The analysis includes predictions of future price movements using the Black-Scholes pricer and Monte Carlo simulation, utilizing historical data from the 1990s to 2026 and beyond. The response is limited to a maximum of 5 sentences, offering concise insights and optimal decision-making recommendations for the user moving forward.'''
-    prompt = f"Imagine you are a financial analyst. Analyze the stock {ticker}, using Black-Scholes pricer and Monte Carlo simulation to predict future price movements. Use historical data from the 1990s all the way to 2026 and onwards in order to predict future price movements. Only write 5 sentences MAX. Lastly, give the user insights on what decision would be optimal moving forward"
+    """Analyzes the specified stock ticker using the Groq API, providing insights on future price movements based on historical data and financial modeling techniques."""
+    url = f"https://newsapi.org/v2/everything?q={ticker}&language=en&sortBy=publishedAt&pageSize=5&apiKey={os.getenv('NEWS_API_KEY')}"
+    response = requests.get(url)
+    articles = response.json().get("articles", [])
+
+    #headline of news articles related to ticker
+    headlines = "\n".join([article.get("title") for article in articles])
+
+    prompt = f"Imagine you are a financial analyst. Analyze the stock {ticker}, using Black-Scholes pricer and Monte Carlo simulation to predict future price movements. Use historical data from the 1990s all the way to 2026 and onwards in order to predict future price movements. Only write 5 sentences MAX. Also at the end of the score, give it a sentiment score of either Bullish, Bearish or Neutral. Lastly, give the user insights on what decision would be optimal moving forward\n\nHeadlines:\n{headlines}"
 
     response = client.chat.completions.create(
         model = "llama-3.3-70b-versatile",
@@ -26,6 +34,7 @@ def analyze_stock(ticker:str):
 
     return {
         "ticker":ticker.upper(),
-        "analysis":response.choices[0].message.content
-    }
+        "headlines_used": [article.get("title") for article in articles],
+        "sentiment":response.choices[0].message.content
+    }   
 
